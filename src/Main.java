1a03aa83921ce68f71099e66e3283c0c6b146c69
Vaagn Akopyan <v.hakobjan@gmail.com>
2018-09-24 23:02:48 +0300

    fix problem, add MYBR

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello my baby girl!");
+
+        System.out.println("I added MYBR branch");
+
     }
 }
